e008111c8e11f06a497e620b747dbc0561841aa4
Shagon1k <shagon1k@gmail.com>
2017-12-10 13:12:51 +0300

    Enabled UglifyJS webpack plugin

--- build/request.bundle.js
+++ build/request.bundle.js
@@ -1,18 +1,2 @@
-webpackJsonp([0],{
-
-/***/ 334:
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__config_js__ = __webpack_require__(124);
-
-
-/* harmony default export */ __webpack_exports__["default"] = (function (sourceId) {
-  return fetch('https://newsapi.org/v2/top-headlines?sources=' + sourceId + '&apiKey=' + __WEBPACK_IMPORTED_MODULE_0__config_js__["a" /* API_KEY */]);
-});
-
-/***/ })
-
-});
+webpackJsonp([0],{334:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var s=n(124);t.default=function(e){return fetch("https://newsapi.org/v2/top-headlines?sources="+e+"&apiKey="+s.a)}}});
 //# sourceMappingURL=request.bundle.js.map
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -5,9 +5,9 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 module.exports = merge(common, {
 	devtool: 'source-map',
 	plugins: [
-		//new UglifyJsPlugin({
-		//	sourceMap: true
-		//}),
+		new UglifyJsPlugin({
+			sourceMap: true
+		}),
 		new webpack.DefinePlugin({
 			'process.env.NODE_ENV': JSON.stringify('production')
 		})
